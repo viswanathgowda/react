80287ea475081a00636934208c80d2af940506ca
mattaviswanathgowda <mattaviswanath@giva.co>
2025-03-06 12:47:53 +0530

    implemented the state, setState in message.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
-import { Welcome1, Welcome2 } from "./components/welcome";
-import Greet from "./components/Greet";
-import { Hello } from "./components/Hello";
+// import { Welcome1, Welcome2 } from "./components/welcome";
+// import Greet from "./components/Greet";
+// import { Hello } from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Welcome1 name="Vishwa" designation="SDE" />
+      {/* <Welcome1 name="Vishwa" designation="SDE" />
       <Welcome2 name="kishor" designation="project manager" />
       <Welcome2 name="Vaibhav" designation="SDE">
         <h1>Passing Children Data</h1>
       </Welcome2>
       <Greet name="passing props to class component" />
-      <Hello />
+      <Hello /> */}
+
+      <Message />
     </div>
   );
 }

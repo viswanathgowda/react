471f471f8063df7808e24c5ffe7c2160e1200023
mattaviswanathgowda <mattaviswanath@giva.co>
2025-03-06 15:10:30 +0530

    Event Handling in React

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,28 @@
 import "./App.css";
-import { Welcome1 } from "./components/welcome";
-import Greet from "./components/Greet";
+// import { Welcome1 } from "./components/welcome";
+// import Greet from "./components/Greet";
 // import { Hello } from "./components/Hello";
-import Message from "./components/Message";
-import Count from "./components/Count";
+// import Message from "./components/Message";
+// import Count from "./components/Count";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
 
 function App() {
   return (
     <div className="App">
-      <Welcome1 name="Vishwa" designation="SDE" />
-      <Greet name="passing props to class component" />
+      {/* <Welcome1 name="Vishwa" designation="SDE" />
+      <Greet name="passing props to class component" /> */}
       {/* <Welcome2 name="kishor" designation="project manager" /> */}
       {/*<Welcome2 name="Vaibhav" designation="SDE">
         <h1>Passing Children Data</h1>
       </Welcome2>
       <Hello /> */}
 
-      <Message />
-      <Count />
+      {/* <Message />
+      <Count /> */}
+
+      <FunctionClick />
+      <ClassClick />
     </div>
   );
 }
